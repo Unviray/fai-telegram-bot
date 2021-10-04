497642812be53e028f796570b00d09bb179c4e9e
Unviray <unviray@gmail.com>
2021-10-04 10:08:03 +0300

    Comment unworked code

--- src/main.py
+++ src/main.py
@@ -8,32 +8,32 @@ from slugify import slugify
 
 
 GROUP_NAME = "Fiangonana Ambodirano"
-BOOK_LIST = Path() / "src/book/"
+# BOOK_LIST = Path() / "src/book/"
 app = Client('Ambodirano')
 
 
-@app.on_message(filters.command("andininteny"))
-async def andininteny(ctx, message):
-    boky = message.command[1]
-    toko = int(message.command[2].split(":")[0])
-    andininy = list(map(int, message.command[2].split(":")[1].split(",")))[0]
+# @app.on_message(filters.command("andininteny"))
+# async def andininteny(ctx, message):
+#     boky = message.command[1]
+#     toko = int(message.command[2].split(":")[0])
+#     andininy = list(map(int, message.command[2].split(":")[1].split(",")))[0]
 
-    def book(name:str):
-        book = BOOK_LIST / f"{slugify(name)}.json"
-        with open(book, "r") as fp:
-            loaded = json.load(fp)
+#     def book(name:str):
+#         book = BOOK_LIST / f"{slugify(name)}.json"
+#         with open(book, "r") as fp:
+#             loaded = json.load(fp)
 
-        return loaded
+#         return loaded
 
-    def chap(name:str, chapter:int):
-        return book(name)[str(chapter)]
+#     def chap(name:str, chapter:int):
+#         return book(name)[str(chapter)]
 
-    verset = chap(boky, toko)[str(andininy)]
-    order = book(boky)['meta']['order'] + 1
-    URL = f"https://www.jw.org/finder?wtlocale=MG&pub=bi12&srctype=wol&bible={order:03}{toko:03}{andininy:03}&srcid=share"
+#     verset = chap(boky, toko)[str(andininy)]
+#     order = book(boky)['meta']['order'] + 1
+#     URL = f"https://www.jw.org/finder?wtlocale=MG&pub=bi12&srctype=wol&bible={order:03}{toko:03}{andininy:03}&srcid=share"
 
-    title = f"[{boky.title()} {toko}:{andininy}]({URL})"
-    await message.reply(f"**{title}**\n{verset}", disable_web_page_preview=True)
+#     title = f"[{boky.title()} {toko}:{andininy}]({URL})"
+#     await message.reply(f"**{title}**\n{verset}", disable_web_page_preview=True)
 
 
 # Filter in only new_chat_members updates generated in TARGET chat
