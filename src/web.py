fbe41263d88bf65b5f69f05a74d8c070c42950be
Unviray <unviray@gmail.com>
2021-10-25 15:35:36 +0300

    Add backend of web-ui

--- src/web.py
+++ src/web.py
@@ -1,7 +1,11 @@
 from aiohttp import web
 import socketio
 
-from main import log, group_call, state, app
+from main import log, group_call, state, app, get_id, id_to_name
+from controller import Commands
+
+
+commands = Commands(client=None)
 
 
 socket = socketio.AsyncServer(logger=False, cors_allowed_origins="*")
@@ -17,22 +21,46 @@ async def network_changed_handler(_, is_connected):
 
 @state.on_changed(["member_io"])
 async def member_io_handler(client, peer, enter, changed):
-    log.info("Send member_io signal")
-    user = await client.get_users(peer.user_id)
+    log.debug("Send member_io signal")
+    user = (await id_to_name(client, [get_id(peer)]))[0]
     await socket.emit("member_io", {
-        "username": user.first_name,
+        "name": user,
         "enter": enter
+    })
+
+
+@state.on_changed(["raise_hand_rating"])
+async def raise_hand_handler(client, peer, old, new, changed):
+    log.debug("Send raise_hand signal")
+    user = (await id_to_name(client, [get_id(peer)]))[0]
+    await socket.emit("raise_hand", {
+        "name": user,
+        "raise": new is not None
+    })
+
+
+@state.on_changed(["muted"])
+async def mute_handler(client, peer, old, new, changed):
+    log.debug("Send mute signal")
+    user = (await id_to_name(client, [get_id(peer)]))[0]
+    await socket.emit("muted", {
+        "name": user,
+        "muted": new
     })
 
 
 @socket.on("join")
 async def join_handler(sid, data):
-    state.chat_id = data
-    await group_call.start(state.chat_id)
+    await commands.command_join(data)
+
+@socket.on("mute")
+async def mute_handler(sid, data):
+    await commands.command_mute(data)
 
 
 def main():
     with app:
+        commands.client = app
         web.run_app(web_app)
 
 
